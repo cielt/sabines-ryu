cff49da13fb62c2b072cc9e59cae1588d18ab5bf
cielt <chiayin.l.tu@gmail.com>
2015-10-04 01:36:42 -0400

    registers event custom post type; adds events page template and event feed item template

--- functions.php
+++ functions.php
@@ -30,6 +30,8 @@ function include_font_linowrite() {
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 'include_font_linowrite' );
 
 /* Custom Post Types */
+
+/* CUSTOM TYPE: writing */
 add_action( 'init', 'register_writing', 1);
 function register_writing() {
 	$labels = array( // Used in the WordPress admin
@@ -44,10 +46,6 @@ function register_writing() {
 		'not_found' =>  __('Nothing found'),
 		'not_found_in_trash' => __('Nothing found in Trash')
 	);
-
-	// TODO: add checkboxes for 
-	// exclude from writing feed
-	// include in home page features
 
 	$args = array(
 		'labels' => $labels, // Set above
@@ -67,6 +65,44 @@ function register_writing() {
 	
 	register_taxonomy_for_object_type('category', 'writing');
 	register_taxonomy_for_object_type('post_tag', 'writing');
+	
+	flush_rewrite_rules();
+}
+
+/* CUSTOM TYPE: event */
+add_action( 'init', 'register_event');
+function register_event() {
+	$labels = array( // Used in the WordPress admin
+		'name' => _x('Events', 'post type general name'),
+		'singular_name' => _x('Event', 'post type singular name'),
+		'add_new' => _x('Add New', 'Event'),
+		'add_new_item' => __('Add New Event'),
+		'edit_item' => __('Edit Event'),
+		'new_item' => __('New Event'),
+		'view_item' => __('View Event'),
+		'search_items' => __('Search Events'),
+		'not_found' =>  __('Nothing found'),
+		'not_found_in_trash' => __('Nothing found in Trash')
+	);
+
+	$args = array(
+		'labels' => $labels, // Set above
+		'public' => true, // Make it publicly accessible
+		'rewrite' => array('slug' => '/events'),
+		'hierarchical' => false, // No parents and children here
+		'menu_position' => 6,
+		'has_archive' => true, // Activate the archive
+		'supports' => array('title',
+												'editor',
+												'excerpt',
+												'thumbnail',
+												'custom_fields') // custom: URLs
+	);
+
+	register_post_type( 'event', $args ); // Create the post type, use options above
+	
+	register_taxonomy_for_object_type('category', 'event');
+	register_taxonomy_for_object_type('post_tag', 'event');
 	
 	flush_rewrite_rules();
 }
