fe2a1d6952c905057113d38e99b0d2222d7027d3
cielt <chiayin.l.tu@gmail.com>
2015-10-19 19:53:03 -0400

    create partial for page footer; include footer in front page and default page template

--- front-page.php
+++ front-page.php
@@ -38,33 +38,8 @@ get_header(); ?>
 						wp_reset_postdata(); ?>
 				</div><!-- /.tiles-wrap -->
 			</div><!-- /.hentry -->
-			<!-- Page footer widget blocks -->
-			<div id="nav-below" class="tiles-wrap page-footer">
-				<div class="wrap clear">
-					<div class="tile-widget tile-2x">
-						<h3 class="widget-block-title h-sans">About</h3>
-						<p><strong>Sabine Heinlein</strong> is the author of the narrative nonfiction book <a href="http://www.amazon.com/Among-Murderers-Life-after-Prison/dp/0520272854" target="_blank"><strong><em>Among Murderers: Life After Prison (University of California Press, 2013)</em></strong></a>, which has won the 2014 Gold IPPY award in the category current affairs/social issues.&nbsp;&nbsp;<a class="continue" href="<?php echo site_url(); ?>/about">More <span class="meta-nav">→</span></a></p>
-					</div>
-					<div class="tile-widget">
-						<h3 class="widget-block-title h-sans">Contact</h3>
-						<ul class="no-bullet">
-							<li><span class="content-cta">sabine [at] sabineheinlein [dot] org</span></li>
-							<li><span class="ico-social" title="Twitter" aria-hidden="true">
-									&#61954;<b class="screen-reader-text">Sabine Heinlein on Twitter</b>
-								</span><a href="https://twitter.com/sabineheinlein" target="_blank" class="content-cta">
-								 @sabineheinlein
-								</a>
-							</li>
-						</ul>
-						</div>
-						<div class="tile-widget last">
-						<h3 class="widget-block-title h-sans">Projects</h3>
-						<ul class="no-bullet">
-							<li><a class="content-cta" href="http://www.sunnysidesoundproject.org/" rel="external" target="_blank">Sunnyside Sound Project</a></li>
-						</ul>
-						</div>
-				</div>
-		</div>
+			<!-- page footer -->
+			<?php include '_page-footer-widgets.php' ?>
 
 		<?php else : ?>
 
