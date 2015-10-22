455a5c23656dd8834433848e06c1ea5a73d154ae
cielt <chiayin.l.tu@gmail.com>
2015-10-21 23:17:23 -0400

    refine style of book sidebar; edits to sidebar content

--- _sidebar-book.php
+++ _sidebar-book.php
@@ -6,11 +6,12 @@
 		<h2 class="entry-title feature-title h-sans">Among Murderers: Life After Prison</h2>
 		<span class="tile-meta">University of California Press, 2013</span>
 		<blockquote class="sidebar-quote">
-			<p>&ldquo;Among Murderers is a remarkable achievement, an eye-opening work of journalistic empathy in the best tradition of Katherine Boo, Ted Conover, and Adrian Nicole LeBlanc. In studying the cases of these three just-released convicts, Sabine Heinlein raises significant policy and philosophical questions about crime and punishment and the nature of &lsquo;rehabilitation.&rsquo; More important, by immersing herself in the lives of these killers over the course of several years, and telling their stories, Heinlein reveals Angel, Adam, and Bruce in all their complexity as they make the transition from decades of life in prison to a limited and often frightening kind of freedom on the outside. This is a triumphantly humane work of reporting and storytelling.&rdquo;</p>
+			<p>&ldquo;Among Murderers is a remarkable achievement, an eye-opening work of journalistic empathy in the best tradition of Katherine Boo, Ted Conover, and Adrian Nicole LeBlanc. This is a triumphantly humane work of reporting and storytelling.&rdquo;</p>
 			<cite class="author">Scott Stossel, editor, <em>The Atlantic</em></cite>
 		</blockquote>
 		<div class="sidebar-cta">
-				<a class="content-cta" href="http://www.amazon.com/Among-Murderers-Life-after-Prison/dp/0520272854/" target="_blank">Buy the book</a>
+				<a class="content-cta clear" href="/book" target="_blank">Learn More</a>
+				<a class="content-cta clear" href="http://www.amazon.com/Among-Murderers-Life-after-Prison/dp/0520272854/" target="_blank">Buy the book</a>
 		</div>
 	</div>
 </div>
