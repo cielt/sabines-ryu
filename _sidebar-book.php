42bfa86b37de769181b33d91b563c4bfa3aba157
cielt <chiayin.l.tu@gmail.com>
2015-10-23 11:44:01 -0400

    edit book sidebar copy

--- _sidebar-book.php
+++ _sidebar-book.php
@@ -1,13 +1,13 @@
 <div id="sidebar-book" class="hentry sidebar-col tile-feature clear">
 	<div class="tile-image">
-		<a class="image-frame feature-thumbnail tile-link" href="http://www.amazon.com/Among-Murderers-Life-after-Prison/dp/0520272854/" target="_blank"><img width="200" height="276" src="http://localhost:8888/sabineheinlein/wp-content/uploads/2015/09/home-feature-among-murderers.png" class="attachment-home-feature-tile wp-post-image" alt="Among Murderers book cover"></a>
+		<a class="image-frame feature-thumbnail tile-link" href="http://www.amazon.com/Among-Murderers-Life-after-Prison/dp/0520272854/" target="_blank"><img width="200" height="276" src="<?php echo get_site_url() ?>/wp-content/uploads/2015/09/home-feature-among-murderers.png" class="attachment-home-feature-tile wp-post-image" alt="Among Murderers book cover"></a>
 	</div>
 	<div class="sidebar-info feature-info">
 		<h2 class="entry-title feature-title h-sans">Among Murderers: Life After Prison</h2>
 		<span class="tile-meta">University of California Press, 2013</span>
 		<blockquote class="sidebar-quote">
-			<p>&ldquo;Among Murderers is a remarkable achievement, an eye-opening work of journalistic empathy in the best tradition of Katherine Boo, Ted Conover, and Adrian Nicole LeBlanc. This is a triumphantly humane work of reporting and storytelling.&rdquo;</p>
-			<cite class="author">Scott Stossel, editor, <em>The Atlantic</em></cite>
+			<p>&ldquo;A deeply compassionate book that poses urgent questions about the end product of imprisonment and the social thirst for vengeance.&rdquo;</p>
+			<cite class="author"><em>Kirkus Review</em></cite>
 		</blockquote>
 		<div class="sidebar-cta">
 				<a class="content-cta clear" href="/book" target="_blank">Learn More</a>
