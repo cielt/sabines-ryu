3fceed9e8869bcb14676264d31c9a2e4c8829f45
cielt <chiayin.l.tu@gmail.com>
2015-10-23 12:07:02 -0400

    wrap waypoints in check for book sidebar

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
 (function (window, $) {
-	$(function () {
-		// add fixed
-		var wpSidebarDown = new Waypoint({
-			element: $('#sidebar-book'),
-  		handler: function(direction) {
-    		if (direction === 'down') {
-    			$(this.element).addClass('scroll-fixed');
-    		} 
-  		},
-  		offset: 0
-		});
+  var sabinesRyu = {};
+  sabinesRyu.breakpoints = {};
+  sabinesRyu.breakpoints.SIDEBAR_BP = 1250;  
 
-		// remove
-		var wpSidebarUp = new Waypoint({
-			element: $('#sidebar-book'),
-  		handler: function(direction) {
-    		if (direction === 'up') {
-    			$(this.element).removeClass('scroll-fixed');
-    		} 
-  		},
-  		offset: 0
-		});
-	});
+  $(function () {
+    var $bookSidebar = $('#sidebar-book'),
+    viewW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+    if ($bookSidebar.length && viewW >= sabinesRyu.breakpoints.SIDEBAR_BP) {
+		// add fixed
+    var wpSidebarDown = new Waypoint({
+     element: $bookSidebar,
+     handler: function(direction) {
+      if (direction === 'down') {
+       $(this.element).addClass('scroll-fixed');
+     } 
+   },
+   offset: 0
+ });
+
+  // remove
+  var wpSidebarUp = new Waypoint({
+    element: $bookSidebar,
+    handler: function(direction) {
+      if (direction === 'up') {
+       $(this.element).removeClass('scroll-fixed');
+     } 
+   },
+   offset: 0
+ });
+}
+});
 })(window, window.jQuery);
