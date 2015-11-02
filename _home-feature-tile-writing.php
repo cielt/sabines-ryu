2379acba55fc2f870eae7955908ec73d9c2051f0
cielt <chiayin.l.tu@gmail.com>
2015-11-02 08:53:06 -0500

    edit custom var name in template

--- _home-feature-tile-writing.php
+++ _home-feature-tile-writing.php
@@ -1,6 +1,6 @@
 <article class="tile-feature home-feature clear">
 	<?php // if external URL, open in new tab 
-			$tile_url = get_field('home_page_feature_url');
+			$tile_url = get_field('read_more_url');
 			$tile_url_attrs = '';
 			if (stristr($tile_url, 'http://') !== false) {
 				$tile_url_attrs = 'rel="external" target="_blank"';	
@@ -8,7 +8,7 @@
 	 ?>		
 	<!-- feature thumb -->
 	<div class="tile-image">
-		<a class="image-frame feature-thumbnail tile-link" href="<?php echo get_field('home_page_feature_url') ?>" <?php echo $tile_url_attrs ?>>
+		<a class="image-frame feature-thumbnail tile-link" href="<?php echo get_field('read_more_url') ?>" <?php echo $tile_url_attrs ?>>
 			<?php the_post_thumbnail('home-feature-tile'); ?>
 		</a>
 		<figcaption class="caption">			
@@ -16,11 +16,15 @@
 		</figcaption>
 	</div>
 	<div class="feature-info">
-		<h2 class="entry-title feature-title h-sans"><a href="<?php echo get_field('home_page_feature_url') ?>" <?php echo $tile_url_attrs ?>>
+		<h2 class="entry-title feature-title h-sans"><a href="<?php echo get_field('read_more_url') ?>" <?php echo $tile_url_attrs ?>>
 			<?php the_title() ?></a></h2>
 		<span class="tile-meta feature-pub-title"><?php echo get_field('publisher') ?></span>
-		<div class="feature-detail"><?php the_excerpt() ?></div>
+		<div class="feature-detail">
+			<?php if (get_field('home_page_feature_excerpt')) { 
+				echo get_field('home_page_feature_excerpt'); 
+			} else { the_excerpt(); } ?>
+		</div>
 		<!-- continue -->
-		<a class="continue" href="<?php echo get_field('home_page_feature_url') ?>" <?php echo $tile_url_attrs ?>>Continue reading <span class="meta-nav">&rarr;</span></a>
+		<a class="continue" href="<?php echo get_field('read_more_url') ?>" <?php echo $tile_url_attrs ?>>Continue reading <span class="meta-nav">&rarr;</span></a>
 	</div>
 </article>
